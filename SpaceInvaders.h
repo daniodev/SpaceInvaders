e470cc204bdc3e1ba0b8ccd1828c68f05d5598c0
Dani <daniodev@gmail.com>
2024-01-20 16:04:43 +0100

    Aggiunto file txt di cose da fare Aggiunto file header Aggiunte le funzioni e ordinato il codice

---
+++ SpaceInvaders.h
@@ -0,0 +1,79 @@
+
+
+#define personaggio_PAIR 1
+#define nemiciSprite_PAIR 2
+
+#define altezza 15
+#define lunghezza 31
+
+#define personaggioSprite "X"
+#define nemiciSprite "O"
+#define proiettileSprite "^"
+
+
+
+void gameBox(){
+
+    for (int y = altezza; y >= 0; y--){
+        mvprintw(y, 0, "#");
+        mvprintw(y, lunghezza, "#");
+    }
+    for (int x = 0; x <= lunghezza; x++){
+        mvprintw(0, x, "#");
+        mvprintw(altezza, x, "#");
+    }
+}
+
+
+void movement_left(int x){
+
+    attron(COLOR_PAIR(personaggio_PAIR));
+    mvprintw(altezza - 1, x - 1, personaggioSprite);
+    mvprintw(altezza - 1, x, " ");
+    attroff(COLOR_PAIR(personaggio_PAIR));
+
+}
+
+void movement_right(int x){
+
+    attron(COLOR_PAIR(personaggio_PAIR));
+    mvprintw(altezza - 1, x + 1, personaggioSprite);
+    mvprintw(altezza - 1, x, " ");
+    attroff(COLOR_PAIR(personaggio_PAIR));
+
+}
+
+void shoot(int x){
+
+    for (int i = 1; i < altezza - 2; i++){
+
+    attron(COLOR_PAIR(personaggio_PAIR));
+    mvprintw(altezza - i - 1, x, proiettileSprite);
+    attroff(COLOR_PAIR(personaggio_PAIR));
+    refresh();
+    mvprintw(altezza - i - 1, x, " ");
+    delay_output(50);     
+    }
+}
+
+void game_end(){
+    clear();
+    getch();
+}
+
+void nemiciSpawn(int nemiciy, int NumNemici){
+
+    for (int n = 3; n <= NumNemici;){
+
+    attron(COLOR_PAIR(nemiciSprite_PAIR));
+    mvprintw(nemiciy, lunghezza / 2 - n + 1, nemiciSprite);
+    mvprintw(nemiciy, lunghezza / 2 + n + 1, nemiciSprite);
+    attroff(COLOR_PAIR(nemiciSprite_PAIR));
+    n += 3;
+
+    }
+}
+
+void punteggioUpdate(int punteggio){
+    mvprintw(0, lunghezza + 2, "Punteggio: %d", punteggio);
+}
--- main.c
+++ main.c
@@ -2,13 +2,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#define personaggio_PAIR 2
-#define nemiciF_PAIR 1
-#define altezza 15
-#define lunghezza 31
-#define personaggio "X"
-#define nemiciF "O"
-#define proiettileSprite "^"
+#include "SpaceInvaders.h"
 
 int main()
 {
@@ -20,134 +14,64 @@ int main()
     initscr();
     raw();
     cbreak();
+    noecho();
+    curs_set(0);
+    start_color();
+
     keypad(stdscr, TRUE);
     nodelay(stdscr, TRUE);
-    noecho();
-    //curs_set(0);
-    start_color();
-    init_pair(nemiciF_PAIR, COLOR_RED, COLOR_BLACK);
+
+    init_pair(nemiciSprite_PAIR, COLOR_RED, COLOR_BLACK);
     init_pair(personaggio_PAIR, COLOR_GREEN, COLOR_BLACK);
 
-    clear();
-
-    for (int y = altezza; y >= 0; y--)
-    {
-        mvprintw(y, 0, "#");
-    }
-
-    for (int y1 = altezza; y1 >= 0; y1--)
-    {
-        mvprintw(y1, lunghezza, "#");
-    }
-
-    for (int x = 0; x <= lunghezza; x++)
-    {
-        mvprintw(0, x, "#");
-    }
-
-    for (int x1 = 0; x1 <= lunghezza; x1++)
-    {
-        mvprintw(altezza, x1, "#");
-    }
+    gameBox();
 
     int x = lunghezza / 2 + 1;
 
     attron(COLOR_PAIR(personaggio_PAIR));
-    mvprintw(altezza - 1, x, personaggio);
+    mvprintw(altezza - 1, x, personaggioSprite);
     attroff(COLOR_PAIR(personaggio_PAIR));
 
-    int nemici = 9;
-    int nemicix;
+    int NumNemici = 9;
     int nemiciy = 3;
 
-    // attron(COLOR_PAIR(nemiciF_PAIR));
-    for (int n = 2; n <= nemici;)
-    {
-        // mvhline(nemiciy, lunghezza / 2 - n + 1, nemiciF,COLS);
-        attron(COLOR_PAIR(nemiciF_PAIR));
-        mvprintw(nemiciy, lunghezza / 2 - n + 1, nemiciF);
-        mvprintw(nemiciy, lunghezza / 2 + n + 1, nemiciF);
-        attroff(COLOR_PAIR(nemiciF_PAIR));
-        // mvhline(nemiciy, lunghezza / 2 - n + 1, nemiciF,COLS);
-        n += 2;
-    }
-    // attroff(COLOR_PAIR(nemiciF_PAIR));
+    nemiciSpawn(nemiciy, NumNemici);
 
-    mvprintw(0, lunghezza + 2, "Punteggio: %d", punteggio);
-    
+    punteggioUpdate(punteggio);
 
-    while (running)
-    {
-
+    while (running){
         left_right = getch();
 
-        if (left_right == KEY_LEFT)
-        {
-
-            if (x > 1)
-            {
-                attron(COLOR_PAIR(personaggio_PAIR));
-                mvprintw(altezza - 1, x - 1, personaggio);
-                mvprintw(altezza - 1, x, " ");
-                attroff(COLOR_PAIR(personaggio_PAIR));
-                x--;
+        if (left_right == KEY_LEFT){
+            if (x > 1){
+            movement_left(x);
+            x--;
             }
         }
 
-        if (left_right == KEY_RIGHT)
-        {
-
-            if (x < lunghezza - 1)
-            {
-                attron(COLOR_PAIR(personaggio_PAIR));
-                mvprintw(altezza - 1, x + 1, personaggio);
-                mvprintw(altezza - 1, x, " ");
-                attroff(COLOR_PAIR(personaggio_PAIR));
+        if (left_right == KEY_RIGHT){
+            if (x < lunghezza - 1){
+                movement_right(x);
                 x++;
             }
         }
 
-        if ((left_right == 'q') && (left_right == 'Q'))
-        {
-            running = FALSE;
-            clear();
+        if ((left_right == 'q') || (left_right == 'Q')){
+            game_end();
             break;
         }
 
-        if (left_right == KEY_UP)
-        {
 
-            for (int i = 1; i < altezza - 2; i++)
-            {
-                 
-                attron(COLOR_PAIR(personaggio_PAIR));
-                mvprintw(altezza - i - 1, x, proiettileSprite);
-                attroff(COLOR_PAIR(personaggio_PAIR));
-                refresh();
-                mvprintw(altezza - i - 1, x, " ");
-                delay_output(50);
-                if(i - 1 == nemiciy && x ){
-                    punteggio += 1;
-                }
-                mvprintw(0, lunghezza + 2, "Punteggio: %d", punteggio);
-      
-            }
+        if (left_right == KEY_UP){
+            shoot(x);
         }
 
-        if (left_right == 'r')
-        {
+        if (left_right == 'r'){
 
-            for (int n = 2; n <= nemici;)
-            {
-                attron(COLOR_PAIR(nemiciF_PAIR));
-                mvprintw(nemiciy, lunghezza / 2 - n + 1, nemiciF);
-                mvprintw(nemiciy, lunghezza / 2 + n + 1, nemiciF);
-                attroff(COLOR_PAIR(nemiciF_PAIR));
-                n += 2;
-            }
+            nemiciSpawn(nemiciy, NumNemici);
 
             punteggio = 0;
-            mvprintw(0, lunghezza + 2, "Punteggio: %d", punteggio);
+            punteggioUpdate(punteggio);
         }
 
         refresh();
